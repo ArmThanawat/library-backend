62821ce04b5b0ec6c31e1b48d3b33976e93ccb9b
Thanawat <thanawat.mmvi@gmail.com>
2025-04-24 19:44:19 +0700

    enable-images

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
-
+import * as bodyParser from 'body-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -15,7 +15,15 @@ async function bootstrap() {
     credentials: true,
   });
 
+  app.use(bodyParser.json({
+    limit: '25mb',
+  }));
 
+  app.use(bodyParser.urlencoded({
+    limit: '25mb',
+    extended: true,
+  }));
+  
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
